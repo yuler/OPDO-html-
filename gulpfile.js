1a26d1fa1dd21ecee0019a73f44dcebd096cc7b4
joyful4yu <joyful4yu@gmail.com>
2015-03-16 13:08:27 +0800

    change gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
  
 gulp.task('sass', function () {
     gulp.src('./assets/sass/**/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(minifyCSS())
+        .pipe(concat('all.css'))
         .pipe(gulp.dest('./assets/css'));
 });
 
