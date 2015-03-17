a83a9cd3c4a1a1cc7ae0c8e7f64ef99d412fccd8
joyful4yu <joyful4yu@gmail.com>
2015-03-17 16:14:13 +0800

    commit to vps

---
+++ assets/js/all.js
@@ -0,0 +1,11 @@
+$(function(){
+
+	// $('select.select2').select2();
+
+	$('.bs3-datepicker').datepicker({
+		language: 'zh-CN',
+		autoclose : true,
+		format: 'yyyy-mm-dd',
+		// endDate : '2013-02-02'
+	})
+});
